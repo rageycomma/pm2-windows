c00f0dd68d315a30d1868aa5046f78cfed4046b1
Jordan Ashley Craw <jordan.craw@ascendx.com>
2020-01-10 16:27:03 +0000

    fix: eslint config

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,34 @@
 const gulp = require("gulp");
 const gulpTypescript = require("gulp-typescript");
+const del = require("del");
 
+const COMPILE_OUT_DIR = "dist";
+const TSCONFIG_NAME = "tsconfig.json"
+
+/**
+ * Builds out the typescript source code.
+ *
+ * @returns
+ */
 function buildTypescript() {
-    const project = gulpTypescript.createProject("tsconfig.json")
+    const gulpTSProject = gulpTypescript.createProject(TSCONFIG_NAME);
 
     return gulp.src("src/**/*.ts")
-        .pipe(typescript({
-
-        }))
+        .pipe(gulpTSProject())
+        .pipe(gulp.dest(COMPILE_OUT_DIR));
 }
+
+/**
+ * Clears the build directory so that sources are up-to-date.
+ */
+async function clearBuildDirectory() {
+    return del([COMPILE_OUT_DIR]);
+}
+
+// Definition of Gulp tasks
+gulp.task("buildTypescript", buildTypescript);
+gulp.task("clearBuildDirectory", clearBuildDirectory);
+
+module.exports = {
+    default: gulp.series("clearBuildDirectory", "buildTypescript"),
+};
